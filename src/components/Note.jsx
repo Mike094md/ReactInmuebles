22267f48dcf4d1cb1ef8e70d1415716aebf7510b
Miguel Diez <miguel.diezj@alumnos.upm.es>
2022-11-21 20:31:30 +0100

    adding notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,10 @@
 
-import { RiDeleteBin6Line } from 'react-icons/ri'
 
 const Note = ({ note, toggleImportance, deleteNote }) => {
   const labelImportance = note.important ? "Important" : "Not important";
 
   return (
     <li>
-      <RiDeleteBin6Line onClick={() => deleteNote(note.id)} style={{ marginRight: '12px', color: 'tomato', cursor: 'pointer'}}/>
       {note.content}
       <button
         className={`btn btn-${
